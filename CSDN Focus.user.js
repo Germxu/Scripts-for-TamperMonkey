be9d1194cc29caabcf00d6359ae9a83a914bdced
Germxu <Germxu@users.noreply.github.com>
2021-01-18 00:39:17 +0800

    Update CSDN Focus.user.js
    add description

--- CSDN Focus.user.js
+++ CSDN Focus.user.js
@@ -2,23 +2,22 @@
 // @name         CSDN Focus
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  try to take over the world!
+// @description  CSS加载, 页面不在再重绘
+// @description  只显示页面文章和评论, 其他全部隐藏
 // @author       Finn
-// @match       https://blog.csdn.net/*/article/*
+// @match       blog.csdn.net/*/article/*
 // @grant       GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    const hideFucks =`
-#csdn-toolbar,#blogColumnPayAdvert, .csdn-side-toolbar, aside, #dmp_ad_58, .recommend-box, .login-mark, #passportbox{
-display:none!important;}
-.d-flex{display:block!important}
-main{width:100%!important; box-shadow: 0 0 50px #69808b78;}
-#mainBox{margin:30px auto;width:1000px!important}
-`
-       GM_addStyle(hideFucks)
-   // document.querySelector("body").insertAdjacentHTML("beforebegin",hideFucks);
-    //document.querySelector(".d-flex").setAttribute("style","display:block!important");
+    const hideChaos =  `
+                       #csdn-toolbar,#blogColumnPayAdvert, .csdn-side-toolbar, aside, #dmp_ad_58, .recommend-box, .login-mark, #passportbox
+                        {display:none!important;}
+                       .d-flex{display:block!important}
+                       main{width:100%!important; box-shadow: 0 0 50px #69808b78;}
+                       #mainBox{margin:30px auto;width:1000px!important}
+                      `;
+    
+   GM_addStyle(hideChaos)
 })();
