8c6b58f8c2c4ad8fb2f3eac44c85a0114b4ec80d
Fengxu <germxu@gmail.com>
2021-01-19 13:03:17 +0800

    bugfix: 大屏幕右侧边栏隐藏

--- CSDN Focus.user.js
+++ CSDN Focus.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         CSDN Focus
 // @namespace    https://github.com/Germxu
-// @version      0.3
-// @description  纯CSS 展开页面文章和全部带翻页评论, 其他全部隐藏, 只在CSDN文章页运行, 沉浸式体验|页面start状态注入样式, 页面不重绘, 提升观感. 因此本脚本不会使用js修改功能, 如有需求会另行开发js增强版本,欢迎👏提交意见反馈,顺便点个star👍...
+// @version      0.5
+// @description  💡: 页面不重绘不闪屏! CSDN无登录弹窗无广告骚扰无推荐阅读! 纯CSS注入, 自动展开CSDN文章和全部评论,页面不重绘! 其他全部隐藏,只在CSDN文章页运行,沉浸式体验|受够了脚本注入导致的闪屏重绘页面吗, 试试不一样的感觉吧 😁
 // @description:en   CSDN cleaner with css. If it help, star on github will be excellent 😄
 // @author       Finn
 // @homepage     https://github.com/Germxu/Scripts-for-TamperMonkey
@@ -24,22 +24,27 @@
     */
     'use strict';
     const hideChaos = `
-                       #csdn-toolbar, #blogColumnPayAdvert, .csdn-side-toolbar, aside, #dmp_ad_58,
+                       #csdn-toolbar .toolbar-advert,#csdn-toolbar .toolbar-container-left,#csdn-toolbar .toolbar-container-right,
+                       .toolbar-search-drop-menu.toolbar-search-half, ::-webkit-input-placeholder, #placeholder
+                       #blogColumnPayAdvert, .csdn-side-toolbar, aside, #dmp_ad_58,
                        .recommend-box, .login-mark, .blog-footer-bottom, .template-box,.leftPop,
                        #toolBarBox, .comment-edit-box.d-flex, #passportbox, .opt-box.text-center,
-                       .hide-article-box.hide-article-pos.text-center
-                        {display:none!important;}
+                       .hide-article-box.hide-article-pos.text-center, #rightAside
+                        {display:none!important; color: transparent; visible:hidden}
+                        .toolbar-search.onlySearch{transition:all 0.3s ease;}
+                       body #csdn-toolbar{box-shadow: 0 2px 10px 0 rgba(0,0,0,.15);position:fixed !important;top: 0px;left: 0px;width: 100%;z-index: 1993;}
+                        .toolbar-search.onlySearch:focus-within {max-width:1000px!important; width:1000px!important}
                        .d-flex{display:block!important}
                        .main_father{height: auto !important;}
                        main{width:100%!important; box-shadow: 0 0 30px #959fa378; margin-bottom:0!important;}
-                       #mainBox{margin:30px auto 50px; width:1000px!important}
+                       #mainBox{margin:50px auto; width:1000px!important}
                        .comment-list-box{max-height:none!important}
-                       #commentPage{display:block!important}
+                       #commentPage, .toolbar-container-middle{display:block!important}
                        #article_content{height:auto !important}
                        .comment-list-container{padding: 4px 0!important}
                        .article-header-box{padding-top: 18px !important}
                        main .comment-box{padding: 0;box-shadow: 0 0 10px rgba(0,0,0,0.05);margin:8px 0;}
                       `;
 
-    GM_addStyle(hideChaos)
+    GM_addStyle(hideChaos);
 })();
