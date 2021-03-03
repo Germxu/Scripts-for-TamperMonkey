57e915f5efb465a2f4b405a757afb8827695240e
Fengxu <germxu@gmail.com>
2021-03-03 22:08:11 +0800

    videoCapture

--- src/视频截图.user.js
+++ src/视频截图.user.js
@@ -1,35 +1,66 @@
 // ==UserScript==
-// @name:cn-ZH       视频截图
-// @name:en         videoCapture
-
-// @namespace    http://tampermonkey.net/
-// @version       0.1
-// @description  视频原始尺寸截图, 支持各大视频站, 支持快捷键 (Alt+K) 连续截屏和按钮动效截屏.
+// @name:cn-ZH   视频截图
+// @name:en      videoCapture
 // @author       Finn
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  视频原始尺寸截图, 支持各大视频站, 支持快捷键 (Alt+K) 连续截屏和按钮动效截屏.
 // @run-at       document-start
-// @match        https://www.bilibili.com/video/BV*
+// @match        https://*/*
 // @grant        GM_openInTab
 // ==/UserScript==
 
 (function () {
     'use strict';
-    // Your code here...
-    //外链直达, 新页面打开
+
     document.addEventListener("DOMContentLoaded", function () {
 
         const v = document.querySelector("video");
         //v.offset.left
-        console.log("视频截图", v)
-        console.log("视频截图", v.offsetLeft, v.offsetHeight, v.offsetWidth);
+        let vs = document.querySelectorAll("video").length;
+        console.log("dom完成", vs);
+        if (vs === 0) {     //沒有video存在, 开启监听Observe
 
-        document.onkeydown = function (e) {
+            let observer = new MutationObserver(function (mutations) {
+                //console.log("mutations",mutations);
+                let v = document.querySelector("video");
+                if(v){
+                    observer.disconnect()
+                    console.log('Observe Video', v);
+                   console.log("视频截图", v.offsetLeft, v.offsetHeight, v.offsetWidth);
+
+                }
+
+            })
+
+            let obTarget = document.querySelector("body");
+            let obConfig = {
+                childList: true,
+                subtree: true
+            }
+            observer.observe(obTarget, obConfig);
+        } else if (vs === 1) {  //一个video
+            console.log("视频截图", vs.offsetLeft, vs.offsetHeight, vs.offsetWidth);
+
+        } else { //video不只有一个, 另行考虑
+
+        }
+
+
+
+
+
+
+
+
+
+        document.onkeydown = function (e) {  //快捷键
             console.log(334, e)
             if (e.altKey && (e.key == "k")) {
                 console.log("ALT+K 快捷键")
                 makeCapture()
             }
         }
-
         function makeCapture() {
             //视频截图
             let canvas = document.createElement("canvas");
