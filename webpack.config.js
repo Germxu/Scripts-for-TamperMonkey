041b533509f82aaa4c847325fdf783a6e45dbc96
Germxu <germxu@gmail.com>
2021-05-04 22:08:24 +0800

    webpack 配置-0

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: {
+        CSDN: './build/CSDN Focus.user.js',
+        //none: './raw/vendor.js',
+      },
+  module: {
+    loaders: [
+      {
+        test: /.s?css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
