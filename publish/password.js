9c75a9d58bd65299198e9fc08106943a2dc01df1
Fengxu <fengxu@enjoymi.com>
2022-03-09 12:56:35 +0800

    update password js

--- publish/password.js
+++ publish/password.js
@@ -2,14 +2,13 @@
 // @namespace    https://github.com/Germxu
 // @homepage     https://github.com/Germxu/Scripts-for-TamperMonkey
 // @supportURL   https://github.com/Germxu/Scripts-for-TamperMonkey/issues/new
-// @version      1.0
+// @version      1.1.2
 // @author       Finn
+// @license      BSD
 // @name         Show Password by double-click
-
 // @name:zh-CN     查看密码
-// @description   😎 Show password By double-click, and auto hide after 5 seconds, also hide when it blur
+// @description   😎 Show password by double-click, and auto hide after 5 seconds, also hide when it blurs
 // @description:zh-CN  😎双击显示密码, 5秒自动隐藏, 失去焦点自动隐藏
-
 // @include     *
 // @grant        none
 // ==/UserScript==
@@ -19,9 +18,11 @@
     document.addEventListener("dblclick", e => {
         const ev = e.target;
         if (ev.nodeName === "INPUT" && ev.getAttribute("type") === "password") {
+            const v = ev.value
             ev.setAttribute("type", "text");
+            ev.value = v
             setTimeout(() => { ev.setAttribute("type", "password") }, 5000)
-            ev.onblur = function () { ev.setAttribute("type", "password") }
+            ev.onblur= () => ev.setAttribute("type", "password")
         }
     })
 })();
