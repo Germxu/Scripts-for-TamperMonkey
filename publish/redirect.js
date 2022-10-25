23e0d84b7272b4d1b8cd66ec6029645f918f1ee2
Fengxu <fengxu@enjoymi.com>
2022-10-25 15:59:22 +0800

    一键直达

---
+++ publish/redirect.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         Redirect2target
+// @name:zh-CN   跳转直达
+// @namespace    https://github.com/Germxu
+// @homepage     https://github.com/Germxu/Scripts-for-TamperMonkey
+// @supportURL   https://github.com/Germxu/Scripts-for-TamperMonkey/issues/new
+// @version      0.1
+// @description  redirect to the target page directly
+// @description:zh-CN  跳转直达, 干掉中间页
+// @author       Finn
+// @match        http://*/*
+// @match        https://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license      MIT
+// @compatible   Chrome 49+
+// @compatible   Safari Latest
+// @compatible   Firefox Latest
+// @run-at       document-start
+// ==/UserScript==
+
+// const reg = /https?:\/\/(url|to|target|redirect|links?).+\/\?(url|to|target|redirect|links?)=(.*?)/
+
+(function () {
+  'use strict';
+  const maybeKeys = ['url', 'to', 'u', 'link', 'links', 'redirect', 'target', 't'].join('|')
+  const reg = new RegExp(`^https?://(${maybeKeys}).+\?(${maybeKeys})=(.*?)$`)
+  const url = location.href
+  if (reg.test(url)) {
+    const redirectkey = url.match(reg)[2]
+    const redirectValue = new URLSearchParams(location.search).get(redirectkey)
+    // console.log("target: ", redirectkey, redirectValue)
+    location.href = redirectValue
+  }
+})();
